d079631cfc2fc56ffc41371bd868b8920d2ea52a
Oerd Cukalla <rosaccu@gmail.com>
2025-04-27 19:37:25 +0200

    Initial stab at QA

--- src/django_scalar/urls.py
+++ src/django_scalar/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-from .scalar import scalar_viewer
+from .views import scalar_viewer
 
 
 app_name = "django_scalar"
 
 urlpatterns = [
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"), # im using drf_spectacular and is the one that i tested with. ps: this endpoint need to be the same from {openapi_url}
-    path(
+    # Endpoint below needs to match {openapi_url} from views.scalar_viewer
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(  # FIXME: this is not needed but if you want, you can keep it.
         "api/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
-    ), # is not needed but if you want, you can keep it.
-    path("api/docs/", scalar_viewer, name="docs"), # scalar view.
+    ),
+    path("api/docs/", scalar_viewer, name="docs"),  # scalar view.
 ]
